580f25efdddaaaa13a3465b4c2cf92b24de050a5
Martin Riehn Madsen <mart112f@zbc.dk>
2023-03-06 08:45:02 +0100

    removed ignore

---
+++ Door2DoorLib/Logs/FileLog.cs
@@ -0,0 +1,27 @@
+﻿using Door2DoorLib.Interfaces;
+
+namespace Door2DoorLib.Logs
+{
+    internal class FileLog : ILog
+    {
+        #region Fields
+        private string _logLocation;
+        private string _message;
+        #endregion
+
+        #region Constructor
+        public FileLog(string message, string logLocation)
+        {
+            _logLocation = logLocation;
+            _message = message;
+        }
+        #endregion
+
+        #region Write Log
+        public void WriteLog()
+        {
+            File.AppendAllText(_logLocation, _message + "\n");
+        }
+        #endregion
+    }
+}
