24ecf3ce3ddb5e30b2de3b8ec62865dc89d8423c
Martin Riehn Madsen <mart112f@zbc.dk>
2023-03-07 08:29:43 +0100

    admin get by username

--- Door2DoorLib/Repositories/AdminRepository.cs
+++ Door2DoorLib/Repositories/AdminRepository.cs
@@ -1,4 +1,6 @@
-﻿using Door2DoorLib.Interfaces;
+﻿using Door2DoorLib.DataModels;
+using Door2DoorLib.Interfaces;
+using MySql.Data.MySqlClient;
 
 namespace Door2DoorLib.Repositories
 {
@@ -13,6 +15,27 @@ namespace Door2DoorLib.Repositories
         {
             _database = database;
         }
+        #endregion
+
+        #region Methods
+        #region Get By User Name
+        public Task<Admin> GetByUserName(string userName)
+        {
+            string query = $"";
+            MySqlCommand sqlCommand = new MySqlCommand(query);
+            Admin result = null;
+            using (var streamReader = _database.ExecuteCommandAsync(sqlCommand).Result)
+            {
+                // Create a new admin from the datastream
+                while (streamReader.HasRows)
+                {
+                    result = new Admin(0, streamReader.GetString("username"), streamReader.GetString("password"));
+                    streamReader.Read();
+                }
+            }
+            return Task.FromResult(result);
+        }
+        #endregion
         #endregion
     }
 }
