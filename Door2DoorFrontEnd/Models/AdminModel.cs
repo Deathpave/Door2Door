3e8a1310c860d111dc460735fd32c65bc47a2a25
Olivervesth <Olivervesth>
2023-03-13 13:00:27 +0100

    fixing frontend admin

--- Door2DoorFrontEnd/Models/AdminModel.cs
+++ Door2DoorFrontEnd/Models/AdminModel.cs
@@ -10,6 +10,12 @@ namespace Door2DoorFrontEnd.Models
 
         public Door2DoorLib.DataModels.Route newroute { get; set; }
 
+        public string FilePath { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileExtension { get; set; }
+
 
     }
 
