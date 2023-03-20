9b13e7c30800cc7a6b8f449ea4185bbfa0fab674
Deathpave <49186768+Deathpave@users.noreply.github.com>
2023-03-20 12:44:36 +0100

    added iform collections, added setauth, added setdata

--- Door2DoorFrontEnd/Models/Auth.cs
+++ Door2DoorFrontEnd/Models/Auth.cs
@@ -2,10 +2,20 @@
 {
     public class Auth
     {
+        #region Properties
         public string Username { get; set; }
 
         public string Password { get; set; }
 
         public int Authenticated { get; set; }
+        #endregion
+
+        #region Constructor
+        public Auth(string userName, int authenticated)
+        {
+            Username = userName;
+            Authenticated = authenticated;
+        }
+        #endregion
     }
 }
